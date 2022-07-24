148307dfdb6ebc02ab7bb4444ae8ca7e97a7e13a
jjs9674 <98516417+jjs9674@users.noreply.github.com>
2022-07-24 20:21:58 +0900

    심화문제 5-3번

--- 기초 및 심화문제/05_심화(2)/05_심화(2)/Program.cs
+++ 기초 및 심화문제/05_심화(2)/05_심화(2)/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("주머니에는 동전이 10개 들어 있습니다.");
+Console.WriteLine("어머니는 몇 개의 동전을 주머니에 넣었나요?");
+string number1 = Console.ReadLine();
+int momPocket = int.Parse(number1);
+Console.WriteLine("아람이는 몇 개의 동전을 꺼냈나요?");
+string number2 = Console.ReadLine();
+int ahRamCoin = int.Parse(number2);
+Console.WriteLine("우람이는 몇 개의 동전을 꺼냈나요?");
+string number3 = Console.ReadLine();
+int wooRamCoin = int.Parse(number3);
+
+Console.Write("주머니에 남아 있는 동전의 개수는 ");
+Console.Write(momPocket - ahRamCoin - wooRamCoin);
+Console.WriteLine("입니다.");
+
