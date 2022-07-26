5c331dcd23ae8d3fbca3ac2fa9fe4de3d03160b4
jjs9674 <98516417+jjs9674@users.noreply.github.com>
2022-07-26 18:14:59 +0900

    조건문 사용(7/26)

--- UltimateBaseball/UitimateBaseball/ConsoleApp1/Program.cs
+++ UltimateBaseball/UitimateBaseball/ConsoleApp1/Program.cs
@@ -17,14 +17,6 @@ Console.WriteLine(number1);
 Console.WriteLine(number2); 
 Console.WriteLine(number3); 
 
-Console.WriteLine("> 수비수가 다시 고른 숫자");
-number1 = 8;
-number2 = 2;
-number3 = 0;
-Console.WriteLine(number1);
-Console.WriteLine(number2);
-Console.WriteLine(number3);
-
 Console.WriteLine("> 첫 번째 숫자를 입력하세요. ");
 int guess1 = int.Parse(Console.ReadLine()); 
 Console.WriteLine("> 두 번째 숫자를 입력하세요. ");
@@ -36,6 +28,45 @@ Console.WriteLine("> 공격수가 고른 숫자");
 Console.WriteLine(guess1);
 Console.WriteLine(guess2);
 Console.WriteLine(guess3);
+
+int strikeCount = 0;
+int ballCount = 0;
+
+if (guess1 == number1)
+{
+    strikeCount = strikeCount + 1;
+}
+else if (guess1 == number2 || guess1 == number3)
+{
+    ballCount = ballCount + 1;
+}
+
+if (guess2 == number2)
+{
+    strikeCount = strikeCount + 1;
+}
+else if (guess2 == number1 || guess2 == number3)
+{
+    ballCount = ballCount + 1;
+}
+
+if (guess3 == number3)
+{
+    strikeCount = strikeCount + 1;
+}
+else if (guess3 == number1 || guess3 == number2)
+{
+    ballCount = ballCount + 1;
+}
+
+Console.Write("스트라이크: ");
+Console.WriteLine(strikeCount);
+Console.Write("볼: ");
+Console.WriteLine(ballCount);
+Console.Write("아웃: ");
+Console.WriteLine(3 - strikeCount - ballCount); 
+
+
 
 
 
