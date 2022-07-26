6a5584d49c0fb7758c0177ffc52eadcd625cd8ba
jjs9674 <98516417+jjs9674@users.noreply.github.com>
2022-07-26 17:14:28 +0900

    7월 26일

---
+++ 06/ComparisonOperators/ComparisonOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("10 > 10 은 ");
+Console.WriteLine(10 > 10); //거짓
+
+Console.Write("10 >= 10 은 ");
+Console.WriteLine(10 >= 10); //참
+
+Console.Write("5 < 10은 ");
+Console.WriteLine(5 < 10); //참
+
+Console.Write("5 <= 10은 ");
+Console.WriteLine(5 < 10); //참
+
+Console.Write("5 == 4는 ");
+Console.WriteLine(5 == 4); //거짓
+
+Console.Write("5 != 4는 ");
+Console.WriteLine(5 != 4); //거짓
+
+
+
---
+++ 06/DoorLock/DoorLock/Program.cs
@@ -0,0 +1,18 @@
+﻿// 디지털 도어락을 구현한 코드
+int passcodeNumbers1 = 6;
+int passcodeNumbers2 = 2;
+
+Console.WriteLine("첫 번째 숫자를 넣어주세요.");
+int userInput1 = int.Parse(Console.ReadLine());
+Console.WriteLine("두 번째 숫자를 넣어주세요.");
+int userInput2 = int.Parse(Console.ReadLine());
+
+if (userInput1 == passcodeNumbers1)
+{
+    if (userInput1 == passcodeNumbers1 && userInput2 == passcodeNumbers2)
+    {
+        Console.WriteLine("문이 열렸습니다.");
+    }
+}
+
+
