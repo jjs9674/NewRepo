98e26f384e128d306bacc27aecb6f2492d2e77c6
jjs9674 <98516417+jjs9674@users.noreply.github.com>
2022-07-27 19:09:02 +0900

    배열 공부

---
+++ 07/Student List/Student List/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("학생 숫자를 입력하세요.");
+int studentCount = int.Parse(Console.ReadLine());
+
+int[] ages = new int[studentCount];
+string[] names = new string[studentCount];
+double[] height = new double[studentCount];
+
+Console.WriteLine("몇 번째 학생의 정보를 추가할까요?");
+int studentNumber = int.Parse(Console.ReadLine());
+
+if (studentNumber >= 0 && studentNumber <= studentCount - 1)
+
+{
+    Console.WriteLine("나이를 입력하세요.");
+    ages[studentNumber] = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("이름을 입력하세요.");
+    names[studentNumber] = Console.ReadLine();
+
+    Console.WriteLine("키를 입력하세요.");
+    height[studentNumber] = double.Parse(Console.ReadLine());
+
+    Console.Write(studentNumber);
+    Console.WriteLine("번째 학생");
+    Console.Write("이름: ");
+    Console.WriteLine(names[studentNumber]);
+    Console.Write("키: ");
+    Console.WriteLine(height[studentNumber]);
+    Console.Write("나이");
+    Console.WriteLine(ages[studentNumber]);
+}
+
+else
+{
+    Console.Write("0에서 ");
+    Console.Write(studentCount - 1);
+    Console.WriteLine("사이의 숫자를 입력하세요.");
+}
+
+
--- UltimateBaseball/UitimateBaseball/ConsoleApp1/Program.cs
+++ UltimateBaseball/UitimateBaseball/ConsoleApp1/Program.cs
@@ -10,26 +10,25 @@ Console.WriteLine(" 숫자만 맞고 순서가 틀리면 볼입니다.");
 Console.WriteLine(" 숫자가 틀리면 아웃입니다.");
 
 Console.WriteLine("> 수비수가 고른 숫자");
-int number1 = 3;
-int number2 = 1;
-int number3 = 9;
-Console.WriteLine(number1);
-Console.WriteLine(number2); 
-Console.WriteLine(number3); 
+int[] numbers = { 3, 1, 9 };
+Console.WriteLine(numbers[0]);
+Console.WriteLine(numbers[1]);
+Console.WriteLine(numbers[2]);
 
+int[] guesses = new int[3];
 Console.WriteLine("> 첫 번째 숫자를 입력하세요. ");
-int guess1 = int.Parse(Console.ReadLine()); 
+guesses[0] = int.Parse(Console.ReadLine()); 
 Console.WriteLine("> 두 번째 숫자를 입력하세요. ");
-int guess2 = int.Parse(Console.ReadLine());
+guesses[1] = int.Parse(Console.ReadLine());
 Console.WriteLine("> 세 번째 숫자를 입력하세요.");
-int guess3 = int.Parse(Console.ReadLine());
+guesses[2] = int.Parse(Console.ReadLine());
 
 Console.WriteLine("> 공격수가 고른 숫자");
-Console.WriteLine(guess1);
-Console.WriteLine(guess2);
-Console.WriteLine(guess3);
+Console.WriteLine(guesses[0]);
+Console.WriteLine(guesses[1]);
+Console.WriteLine(guesses[2]);
 
-if (guess1 == guess2 || guess1 == guess3 || guess2 == guess3)
+if (guesses[0] == guesses[1] || guesses[0] == guesses[2] || guesses[1] == guesses[2])
 {
     Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
 }
@@ -38,29 +37,29 @@ else
     int strikeCount = 0;
     int ballCount = 0;
 
-    if (guess1 == number1)
+    if (guesses[0] == numbers[0])
     {
         strikeCount = strikeCount + 1;
     }
-    else if (guess1 == number2 || guess1 == number3)
+    else if (guesses[0] == numbers[1] || guesses[0] == numbers[2])
     {
         ballCount = ballCount + 1;
     }
 
-    if (guess2 == number2)
+    if (guesses[0] == numbers[1])
     {
         strikeCount = strikeCount + 1;
     }
-    else if (guess2 == number1 || guess2 == number3)
+    else if (guesses[1] == numbers[0] || guesses[1] == numbers[2])
     {
         ballCount = ballCount + 1;
     }
 
-    if (guess3 == number3)
+    if (guesses[2] == numbers[2])
     {
         strikeCount = strikeCount + 1;
     }
-    else if (guess3 == number1 || guess3 == number2)
+    else if (guesses[2] == numbers[0] || guesses[2] == numbers[1])
     {
         ballCount = ballCount + 1;
     }
